03fb4b7a03c207e357796e07ae6cfa5afb8ca733
KowshalyaBoopathi <kowshalyaboopathi98@gmail.com>
2021-09-24 15:09:06 +0530

    test

---
+++ ReverseNumber.java
@@ -0,0 +1,30 @@
+package com.logical.programming;
+
+import java.util.*;
+
+public class ReverseNumber {
+	int number;
+	
+	void reverse(int n) {int rev=0,rem;
+		/*if(n<10) {
+			System.out.print(n);
+		}
+		else {
+			System.out.print(n%10);
+			reverse(n/10);
+		}*/
+		while(n!=0) {
+			rem=n%10;
+			rev=rev*10+rem;
+			n=n/10;
+		}
+		System.out.print(rev);
+	}
+	public static void main(String args[]) {
+		System.out.println("Enter any number");
+		Scanner scan=new Scanner(System.in);
+		int num=scan.nextInt();
+		ReverseNumber number=new ReverseNumber();
+		number.reverse(num);
+	}
+}
