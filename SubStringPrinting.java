6cf9dcffdfd2d6b35b9a67f2a8dc69c3da0c426f
KowshalyaBoopathi <kowshalyaboopathi98@gmail.com>
2021-09-28 10:13:52 +0530

    test

--- SubStringPrinting.java
+++ SubStringPrinting.java
@@ -5,65 +5,63 @@ import java.util.*;
 
 public class SubStringPrinting {
 
-	/*public static final int CHARACTER_RANGE = 128;
+	public static final int CHARACTER_RANGE = 128;
 	 
     public static String Substring(String str)
     {
-        if (str == null || str.length() == 0) {
+        if (str==null||str.length()==0) {
             return str;
         }
-         boolean[] now = new boolean[CHARACTER_RANGE];
+         boolean[] now=new boolean[CHARACTER_RANGE];
  
-        int begin = 0, end = 0;
+        int begin=0, end=0;
  
-        for (int low = 0, high = 0; high < str.length(); high++)
+        for (int low=0, high=0; high<str.length(); high++)
         {
             if (now[str.charAt(high)])
             {
-                while (str.charAt(low) != str.charAt(high))
+                while (str.charAt(low)!=str.charAt(high))
                 {
-                    now[str.charAt(low)] = false;
+                    now[str.charAt(low)]=false;
                     low++;
                 }
                 low++;        
             }
             else {
-
-                now[str.charAt(high)] = true;
- 
-                if (end - begin < high - low)
+                now[str.charAt(high)]=true;
+                if (end-begin<high-low)
                 {
-                    begin = low;
-                    end = high;
+                    begin=low;
+                    end=high;
                 }
             }
         }
-         return str.substring(begin, end + 1);
-    }*/
+         return str.substring(begin, end+1);
+    }
 	
     /*public void Substring(StringBuffer str) {
     	int size=str.length();
     	System.out.println(size);
     	
-    }
+    }*/
 
  
     public static void main(String[] args)
     {
-        StringBuffer str =new StringBuffer("However Everyday is a Day");
+        //StringBuffer str =new StringBuffer("However Everyday is a Day");
         //StringBuffer str =new StringBuffer("Happy Holi!");
-        
-        SubStringPrinting sub=new SubStringPrinting();
-        sub.Substring(str);
-     }*/
+    	
+    	//String str= "However Everyday is a Day";
+    	String str="happy Holi";
+    	        
+        //SubStringPrinting sub=new SubStringPrinting();
+        System.out.println(Substring(str));
+     }
 	
-	// Java implementation to print the longest common substring
 
-		/* function to find and print the longest common
-		substring of X[0..m-1] and Y[0..n-1] */
-		static void printSubStr(String X, String Y, int m, int n)
+		/*static void printSubStr(String X, String Y, int m, int n)
 		{
-			int[][] LCSuff = new int[m + 1][n + 1];
+			int[][] string = new int[m + 1][n + 1];
 
 			int len = 0;
 
@@ -72,28 +70,28 @@ public class SubStringPrinting {
 			for (int i = 0; i <= m; i++) {
 				for (int j = 0; j <= n; j++) {
 					if (i == 0 || j == 0)
-						LCSuff[i][j] = 0;
+						string[i][j] = 0;
 
 					else if (X.charAt(i - 1) == Y.charAt(j - 1)) {
-						LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
-						if (len < LCSuff[i][j]) {
-							len = LCSuff[i][j];
+						string[i][j] = string[i - 1][j - 1] + 1;
+						if (len < string[i][j]) {
+							len = string[i][j];
 							row = i;
 							col = j;
 						}
 					}
 					else
-						LCSuff[i][j] = 0;
+						string[i][j] = 0;
 				}
 			}
 
 			if (len == 0) {
-				System.out.println("No Common Substring");
+				System.out.println("none");
 				return;
 			}
 			String resultStr = "";
 
-			while (LCSuff[row][col] != 0) {
+			while (string[row][col] != 0) {
 				resultStr = X.charAt(row - 1) + resultStr; 
 				--len;
 
@@ -124,5 +122,5 @@ public class SubStringPrinting {
 			int n = Y.length();
 
 			printSubStr(X, Y, m, n);
-		}
+		}*/
 }
