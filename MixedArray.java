ffe8d0ae2005c57fec31422ba70013326b23b03e
KowshalyaBoopathi <kowshalyaboopathi98@gmail.com>
2021-09-29 08:37:42 +0530

    changed

--- MixedArray.java
+++ MixedArray.java
@@ -32,8 +32,9 @@ public class MixedArray {
 		array[b] = temp;
 	}*/
 	
+	
 	public void arrangeArray(char array[],int size) {
-		int cred=0,cblue=0,cgreen=0,count=0,i=0;
+		int cred=0,cblue=0,cgreen=0,count=0,i=0,j=0;
 		for(i=0;i<size;i++) {
 		switch(array[i]) {
 		case 'R':
@@ -48,23 +49,37 @@ public class MixedArray {
 		}
 	}
 		System.out.println(cred+""+cblue+""+cgreen);
+		
+		int a=0,b=0,c=0;
+		
 			while(count<size) {
-				char ch=array[count];
-				if(ch=='B') {
+				
+				char ch=array[j];
+				
+				if((ch=='B')&&(a==0)) {
+					++a;
 					print(ch,cblue);
 					count+=cblue;
-					
+					j++;
 				}
-				else if(ch=='G') {
+				else if((ch=='G')&&(b==0)) {
+					++b;
 					print(ch,cgreen);
 					count+=cgreen;
+					j++;
 				}
-				else if(ch=='R') {
+				else if ((ch=='R')&&(c==0)) {
+					++c;
 					print(ch,cred);
 					count+=cred;
+					j++;
 				}
+				else 
+					j++;
 			}
 }
+	
+	
 	void print(char ch,int n) {
 		while(n!=0) {
 			System.out.println(ch);
@@ -75,8 +90,8 @@ public class MixedArray {
 	
 	public static void main (String[] args)
 	{
-		//char array[] = {'R', 'G', 'B', 'G', 'B', 'G', 'R'};
-		char array[] = {'B', 'G', 'B', 'R', 'G', 'R', 'B', 'G', 'R', 'R', 'B'};
+		char array[] = {'B', 'R', 'B', 'G', 'B', 'G', 'R'};
+		//char array[] = {'B', 'G', 'B', 'R', 'G', 'R', 'B', 'G', 'R', 'R', 'B'};
 		int size = array.length;
 
 		MixedArray sub = new MixedArray();
