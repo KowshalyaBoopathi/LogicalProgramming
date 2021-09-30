1bd2239b1b8f48fb4a6db60d42309eb572bb3df3
KowshalyaBoopathi <kowshalyaboopathi98@gmail.com>
2021-10-01 00:03:01 +0530

    trap

--- TrapNumbers.java
+++ TrapNumbers.java
@@ -4,20 +4,14 @@ import java.util.*;
 
 public class TrapNumbers {
 	
-	boolean traps(int n1, int n2, int y) {
-			
-			for(int i=n1;i<=n2;i++) {
-				int z=sumOfDigits(i);
-				if(i==y) {
-					return true;
-				}
-				if(i==z) {
-					return true;
-				}
-			}
-		return false;
+	int yCheck(int y) {
+		if(y<3)
+			y=3;
+		else
+			y=y;
+		return y;
 	}
-	
+
 	int sumOfDigits(int number) {
 		int sum=0;
 		while(number!=0) {
@@ -28,25 +22,45 @@ public class TrapNumbers {
 		return sum;
 	}
 	
-	/*boolean validate(int n1, int n2, int y) {	
-		if(y>=3)
-			return true;
-		else if(n2>=0&&n1>=0)
-			return true;
-		else
-			return false;
-	}*/
 	void trapSequence(int n1,int n2,int y) {
-		for(int i=n1;i<=n2;i++) {
-			if(traps(n1,n2,y)) {
-				System.out.println(n1);
+		
+		int count=0;
+		
+		for(int x=n1;x<=n2;x++) {
+			//System.out.print(x);
+			int z=sumOfDigits(x);
+
+			if((x==y) || (z==y)) {
 				y=y+2;
+				System.out.println(x);
+				count=count+1;
+			}	
+			
+			else if((x%y==0)||(y%x==0)||(z%y==0)||(y%z==0)) {
+				y=y+2;
+				System.out.println(x);
+				count=count+1;
 			}
-			else if(y>=3){
-				y--;
+			
+			else {
+				y=y-1;
+				y=yCheck(y);
 			}
 		}
+		if(count==0)
+			System.out.println("NONE");
 	}
+	
+	/*int[] toArray(int n1, int n2) {
+		int n=n2-n1+1;
+		int [] array=new int [n];
+		for(int i=0;i<n;i++) {
+			array[i]=n1++;
+		}
+		/*for(int i=0;i<n;i++)
+			System.out.println(array[i]);*/
+		/*return array;
+		}*/
 	
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
@@ -57,7 +71,7 @@ public class TrapNumbers {
 		System.out.println("Enter y value");
 		int y=scan.nextInt();
 		TrapNumbers obj =new TrapNumbers();
-		obj.trapSequence(n1, n2, y);
+		obj.trapSequence(n1,n2,y);
 	}
-
 }
+
